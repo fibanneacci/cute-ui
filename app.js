82ba88f827c8322305d1b6ea3833b05f0792e562
Anne Li <24998524+fibanneacci@users.noreply.github.com>
2020-12-19 18:16:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 document.getElementById("submit_button").onclick = function () {
   let textarea = document.getElementById("input_message")
   let input = textarea.value
-  textarea.value = input
 };
